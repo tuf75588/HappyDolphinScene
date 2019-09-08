3fa82764b44fd4ac00fb98d3c33d8a857cec9045
Andrew Davis <atd285@gmail.com>
2019-09-08 15:10:01 -0400

    its usually a good idea to make sure your js file isn't commented out :smile:

--- index.js
+++ index.js
@@ -2,59 +2,46 @@ const emoteRef = document.querySelector('#dolphin-container')
 const halfWidth = 100;
 const fullWidth = halfWidth * 2;
 console.log('connected!')
-// const state = {
-//   position: {
-//     x: (window.innerWidth / 2) - (emoteRef.clientWidth / 2),
-//     y: (window.innerHeight / 2) - (emoteRef.clientHeight / 2),
-//   },
-//   velocity: {
-//     x: 1,
-//     y: 2,
-//   }
-// }
-
-// function draw() {
-//   emoteRef.style.top = `${state.position.y}px`
-//   emoteRef.style.left = `${state.position.x}px`
-// }
-
-// function updatePosition() {
-//   state.position.x += (state.velocity.x * 4);
-//   state.position.y += (state.velocity.y * 4);
-// }
-// //! collision detection
-
-// function bounce() {
-//   //right edge of screen
-//   if (state.position.x + emoteRef.clientWidth >= window.innerWidth) {
-//     state.velocity.x *= -1
-//   } else if (state.position.x <= 0) {
-//     state.velocity.x *= -1
-//   }
-//   if (state.position.y + emoteRef.clientHeight >= window.innerHeight) {
-//     state.velocity.y *= -1;
-//   } else if (state.position.y <= 0) {
-//     state.velocity.y *= -1
-//   }
-// }
-
-// function paint() {
-//   draw();
-//   updatePosition(');
-//   bounce()
-//   requestAnimationFrame(paint)
-// }
-// paint()
-
-const name = "Andrew";
-console.log(name)
-
-class Animal extends Array {
-  constructor(name) {
-    super();
-    console.log('in the constructor')
-    this.name = name
+const state = {
+  position: {
+    x: (window.innerWidth / 2) - (emoteRef.clientWidth / 2),
+    y: (window.innerHeight / 2) - (emoteRef.clientHeight / 2),
+  },
+  velocity: {
+    x: 1,
+    y: 2,
   }
-
 }
-console.log(new Animal)
+
+function draw() {
+  emoteRef.style.top = `${state.position.y}px`
+  emoteRef.style.left = `${state.position.x}px`
+}
+
+function updatePosition() {
+  state.position.x += (state.velocity.x * 4);
+  state.position.y += (state.velocity.y * 4);
+}
+//! collision detection
+
+function bounce() {
+  //right edge of screen
+  if (state.position.x + emoteRef.clientWidth >= window.innerWidth) {
+    state.velocity.x *= -1
+  } else if (state.position.x <= 0) {
+    state.velocity.x *= -1
+  }
+  if (state.position.y + emoteRef.clientHeight >= window.innerHeight) {
+    state.velocity.y *= -1;
+  } else if (state.position.y <= 0) {
+    state.velocity.y *= -1
+  }
+}
+
+function paint() {
+  draw();
+  updatePosition(');
+  bounce()
+  requestAnimationFrame(paint)
+}
+paint()
